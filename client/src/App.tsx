7ae7a2941c97b2c10013787c6b6e77ac55fe5be0
Joseph Waldron <waldrojo@kean.edu>
2023-03-18 10:36:51 -0400

    respnsive grid

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <p>Hello 123</p>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: '"nav nav nav" "locator main form"',
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "400px 50% 400px",
+      }}
+    >
+      <GridItem gridArea="nav">Nav</GridItem>
+      <GridItem gridArea="locator">locator</GridItem>
+      <Show above="lg">
+        <GridItem gridArea="main">main</GridItem>
+        <GridItem gridArea="form">form</GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
 export default App;
