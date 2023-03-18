78a780c9cea9d6133afa15e1f5b0ad2992139011
Zach Gordon <zachgordon25@gmail.com>
2023-03-18 11:25:50 -0400

    updated history schema

--- server/models/history.js
+++ server/models/history.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoos");
 
 const historySchema = new mongoose.Schema({
   parkingLotId: { type: String, required: true },
-  name: { type: String, required: true },
-  zone: { type: String, required: true },
   dateTime: { type: Date, default: Date.now },
+  averageOccupancy: { type: Number, required: true },
 });
 
 const History = mongoose.model("History", historySchema);
